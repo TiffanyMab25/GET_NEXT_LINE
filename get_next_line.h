95367556f5fa8dea4c0db8d2759b9dfa4ac26bca
Tiffany MABUNDA <tmabunda@c4r8s3.wethinkcode.co.za>
2019-06-25 08:41:27 +0200

    GNL.H

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmabunda <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/25 07:45:43 by tmabunda          #+#    #+#             */
+/*   Updated: 2019/06/25 07:51:22 by tmabunda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE.H
+# define BUFF_SIZE1
+
+# include "libft.h"
+# include "unistd.h"
+# include "stdlib.h"
+
+#endif
