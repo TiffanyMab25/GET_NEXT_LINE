017a2fab61f7e3bdbc0900b866eeaf8b225963bf
Tiffany MABUNDA <tmabunda@c4r8s3.wethinkcode.co.za>
2019-07-03 16:32:49 +0200

    OK

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,28 @@
 /*   By: tmabunda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 11:24:43 by tmabunda          #+#    #+#             */
-/*   Updated: 2019/06/30 14:09:48 by tmabunda         ###   ########.fr       */
+/*   Updated: 2019/07/03 16:31:02 by tmabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h/libft"
 
-int	get_next_line(const int fd, char **line)
+int			ft_readline(char **line, char *str)
 {
-	static char	*c[2147483647];
-	char		buffer[BUFF_SIZE + 1];
-	char		*tmp;
-	ssize_t		b;
-	int			endl;
-
-	if (fd < 0 || (!c[fd] && !(c[fd] = ft_strnew(1))) || !line)
-		return (-1);
-	while (!ft_strchr(c[fd], '\n') && (b = read(fd, buffer, BUFF_SIZE)) > 0)
-	{
-		buffer[b] = '\0';
-		tmp = c[fd];
-		c[fd] = ft_strjoin(c[fd], buffer);
-		ft_strdel(&tmp);
-	}
-	if (b == -1 || !*(tmp = c[fd]))
-		return (b == -1 ? -1 : 0);
-	if ((endl = (ft_strchr(c[fd], '\n') > 0)))
-		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
+if (*str && ft_strlen(*str) > 0)
+	*line = *str;
+	*str = NULL;
+	return (1);
 	else
-		*line = ft_strdup(c[fd]);
-	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
-			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	return (!(!c[fd] && !ft_strlen(*line)));
+		return (0);
 }
+
+int		search_newline(char **line , char *str)
+{
+	char *temp1;
+	char temp2;
+	char c;
+
+	temp1 = str1;
+	temp2 = str2;
+	c = ft_strchar(temp1, '\n');
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,20 @@
 /*   By: tmabunda <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 07:45:43 by tmabunda          #+#    #+#             */
-/*   Updated: 2019/06/30 14:07:59 by tmabunda         ###   ########.fr       */
+/*   Updated: 2019/07/03 15:08:13 by tmabunda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# include <unistd.h>
+# define BUFF_SIZE 100
+#include "libft/libft.h"
+#include <unistd.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
-int					get_next_line(const int fd, char **line);
+
+int		get_next_line(const int fd, char **line);
 
 #endif
